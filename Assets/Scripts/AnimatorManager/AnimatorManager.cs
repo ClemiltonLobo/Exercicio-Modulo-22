7f41f68ce02f4bb62c2dfd4f4fc6fbb7cc3ab6eb
ClemiltonLobo <clemilton.lobo@gmail.com>
2023-06-14 22:42:41 -0300

    Adicionando script que gerencia todas as animações

---
+++ Assets/Scripts/AnimatorManager/AnimatorManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorManager : MonoBehaviour
+{
+    public Animator animator;
+    public List<AnimatorSetup> animatorSetups;
+    public enum AnimationType
+    {
+        IDLE,
+        RUN,
+        DEAD,
+        VICTORY
+    }
+
+    public void Play(AnimationType type, float currentSpeedFactor = 1)
+    {
+        foreach(var animation  in animatorSetups)
+        {
+            if (animation.type == type)
+            {
+                animator.SetTrigger(animation.trigger);
+                animator.speed = animation.speed * currentSpeedFactor;
+                break;
+            }
+        }
+    }
+    public void Update()
+    {
+     if(Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            Play(AnimationType.RUN);
+        }
+     else if(Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            Play(AnimationType.DEAD);
+        }
+     else if (Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            Play(AnimationType.IDLE);
+        }
+     else if (!Input.GetKeyUp(KeyCode.Alpha4))
+        {
+            Play(AnimationType.VICTORY);
+        }
+    }
+}
+[System.Serializable]
+
+public class AnimatorSetup
+{
+    public AnimatorManager.AnimationType type;
+    public string trigger;
+    public float speed = 1f;
+}
